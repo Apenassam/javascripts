4ec5ff9b3ef26995abcc46e3643352aa90d06c1d
Talyta Sampaio <sampaio_talyta@hotmail.com>
2025-05-21 08:10:35 -0400

    put

---
+++ projeto/js/scripts.js
@@ -0,0 +1,12 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Nome:', name);
+    console.log('Email:', email);
+    console.log('Mensagem:', message);
+
+    // Aqui você pode adicionar código para enviar a mensagem ao servidor
+});
